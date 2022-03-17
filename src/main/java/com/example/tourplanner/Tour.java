35a1bbd2bfbc674e20c3ea204097bf1b2fed2b9d
if19b020 <if19b020@technikum-wien.at>
2022-03-17 09:09:27 +0100

    Create Tour.java

---
+++ src/main/java/com/example/tourplanner/Tour.java
@@ -0,0 +1,14 @@
+package com.example.tourplanner;
+
+public class Tour {
+    private String name;
+    private String description;
+    private String from;
+    private String to;
+    private String transport_type;
+    private int distance;
+    private int estimated_time;
+    private String route_information;
+
+
+}
