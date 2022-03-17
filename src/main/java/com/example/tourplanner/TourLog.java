259c0ce5e65ae83903cb2b4ac8b9e22d9e78b555
if19b020 <if19b020@technikum-wien.at>
2022-03-17 09:25:27 +0100

    getters & setters

--- src/main/java/com/example/tourplanner/TourLog.java
+++ src/main/java/com/example/tourplanner/TourLog.java
@@ -9,4 +9,52 @@ public class TourLog {
     private int difficulty; // 1 = easy, 5 = difficult
     private int time;
     private int rating;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
 }
