a073b302ea97b61eeb0c53118ed29c3e47e01f6c
if19b020 <if19b020@technikum-wien.at>
2022-03-17 09:21:52 +0100

    Update TourLog.java

--- src/main/java/com/example/tourplanner/TourLog.java
+++ src/main/java/com/example/tourplanner/TourLog.java
@@ -1,4 +1,12 @@
 package com.example.tourplanner;
 
+import java.util.Date;
+
 public class TourLog {
+    private Date date;
+    private int duration;
+    private String comment;
+    private int difficulty; // 1 = easy, 5 = difficult
+    private int time;
+    private int rating;
 }
