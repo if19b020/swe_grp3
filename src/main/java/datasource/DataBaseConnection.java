e9d80de414535931fabc2bee20aa68c94d612cf3
if19b020 <if19b020@technikum-wien.at>
2022-05-06 14:51:15 +0200

    Update DataBaseConnection.java

--- src/main/java/datasource/DataBaseConnection.java
+++ src/main/java/datasource/DataBaseConnection.java
@@ -20,20 +20,6 @@ public class DataBaseConnection implements IDataBaseConnection {
         Class.forName("org.postgresql.Driver");
         java.util.Properties props = new java.util.Properties();
         // TODO save username + password in config file
-        props.setProperty("user", user);
-        props.setProperty("password", password);
-        /* don't use server prepared statements */
-        props.setProperty("prepareThreshold", "0");
-        /*
-         * Tell the driver manager to connect to the database specified with the URL.
-         * This may throw an SQLException.
-         */
-
-        /*try (Connection conn = DriverManager.getConnection(url, props)) {
-            System.out.println(conn.getMetaData().getDatabaseProductVersion());
-        } catch(SQLException e) {
-            System.out.println("Error connecting to database " + Arrays.toString(e.getStackTrace()));
-        }*/
 
         try {
             return DriverManager.getConnection(connectionString, user, password);
