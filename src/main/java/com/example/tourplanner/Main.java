6cb02a5e7dd209cde4c85d61773788144a48b86d
if19b020 <if19b020@technikum-wien.at>
2022-05-09 19:03:12 +0200

    update main

--- src/main/java/com/example/tourplanner/Main.java
+++ src/main/java/com/example/tourplanner/Main.java
@@ -2,10 +2,12 @@ package com.example.tourplanner;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
     @Override
@@ -18,6 +20,6 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
