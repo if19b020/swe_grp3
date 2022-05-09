e3d001dda5f2dac227263ee57f5debb195749310
if19b020 <if19b020@technikum-wien.at>
2022-05-09 17:09:25 +0200

    reorganizing files

--- src/main/java/views/mainViewController.java
+++ src/main/java/views/mainViewController.java
@@ -1,4 +1,4 @@
-package ViewController;
+package views;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
--- src/main/java/views/tourDescriptionView.java
+++ src/main/java/views/tourDescriptionView.java
@@ -1,4 +1,4 @@
-package TourController;
+package views;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
