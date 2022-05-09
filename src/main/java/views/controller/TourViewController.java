97a74b205c9398db365a70318a194e8735116b25
if19b020 <if19b020@technikum-wien.at>
2022-05-09 18:48:42 +0200

    Update TourViewController.java

--- src/main/java/views/controller/TourViewController.java
+++ src/main/java/views/controller/TourViewController.java
@@ -10,23 +10,23 @@ import views.TourViewModel;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import static javafx.application.Application.launch;
 
 public class TourViewController implements Initializable {
 
-    public TourViewModel tourViewModel;
+    public TourViewModel tourViewModel = new TourViewModel();
+
+    public TextField tourName = new TextField();
+    public TextField from = new TextField();
+    public TextField to = new TextField();
+    public TextField description = new TextField();
     public Button confirmButton;
-    public TextField tourName;
-    public TextField from;
-    public TextField to;
-    public TextField description;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        /*Bindings.bindBidirectional(tourName.textProperty(), tourViewModel.getTourName());
+        Bindings.bindBidirectional(tourName.textProperty(), tourViewModel.getTourName());
         Bindings.bindBidirectional(from.textProperty(), tourViewModel.getFrom());
         Bindings.bindBidirectional(to.textProperty(), tourViewModel.getTo());
-        Bindings.bindBidirectional(description.textProperty(), tourViewModel.getDescription());*/
+        Bindings.bindBidirectional(description.textProperty(), tourViewModel.getDescription());
         // TODO confirmButton
     }
 
